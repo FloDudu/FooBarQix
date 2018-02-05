23df6ec03f89d80de5609605426581672e1bce8f
PC_FLORIAN\Florian <florian.g.duval@gmail.com>
2018-02-05 18:54:56 +0100

    Init solution with 2 projects (FooBarQix.cs and FooBarQixTest.cs)

---
+++ FooBarQix/FooBarQixTest/FooBarQixTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FooBarQixTest
+{
+    [TestClass]
+    public class FooBarQixTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
